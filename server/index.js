ce54a363382d660d1a254b383985d69eca2b2e1f
zain-ali-syed <chelnov63@gmail.com>
2025-12-24 13:20:56 +0000

    updated port

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/todos", requireAuth, todoRoutes);
 
 async function startServer() {
-  const port = 3000;
+  const port = 8080;
   await connectDB();
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
