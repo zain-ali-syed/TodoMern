ac7c602b596b3c8dc267e2847fe4515d832e5c17
zain-ali-syed <chelnov63@gmail.com>
2025-12-26 16:10:22 +0000

    updates

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://todoappv2-757973689656.europe-west1.run.app", // your React dev server or FE container
+    origin: process.env.CLIENT_URL, // your React dev server or FE container
     credentials: true,
   })
 );
